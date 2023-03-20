7e94e6dc4f6465177bc1a7bcb479caf370071f54
TheDusty117 <gabrieledipassio@gmail.com>
2023-03-20 16:26:38 +0100

    FEAT ul for cycle

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,7 +52,89 @@
   <link rel="stylesheet" href="./css/app.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 </head>
-<body>
-  <h2>ciao esercizio pome</h2>
+
+<body class="bg-primary">
+
+<?php
+foreach ($hotels as $key => $value) :
+?>
+
+<li> 
+    <ul>
+      <?php
+      foreach ($value as $hotel) :
+      ?>
+
+        <li><?php echo $hotel; ?></li>
+
+      <?php
+      endforeach;
+      ?>
+
+    </ul>
+</li>
+
+<?php
+endforeach;
+?>
+
+
+
+
+
+
+
+
+
+<main>
+
+  <div class="container">
+  
+    <table class="table">
+
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nome</th>
+          <th scope="col">Descrizione</th>
+          <th scope="col">Parcheggio</th>
+          <th scope="col">Voto</th>
+          <th scope="col">Distanza</th>
+        </tr>
+      </thead>
+
+      
+      <tbody>
+
+      <?php
+      foreach ($hotels as $chiave => $valore):
+      ?>
+
+        <tr>
+          <th>
+            <?php
+            echo $chiave;
+              ?>
+          </th>
+          <td>blabla</td>
+        </tr>
+
+        <tr>
+          <th>2</th>
+          <td>blabla</td>
+        </tr>
+
+
+      <?php
+      endforeach
+      ?>  
+
+      </tbody>
+
+    </table>
+  </div>
+</main>
+
+
 </body>
 </html>
